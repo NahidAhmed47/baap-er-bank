61dc448c051980553333028e6564916778542e91
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-02-12 10:55:04 +0600

    validation login added

---
+++ js/login.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-login').addEventListener('click', function(){
+    const userEmail = document.getElementById('user-email').value;
+    const userPassword = document.getElementById('user-password').value;
+    if(userEmail === 'mybank@gmail.com' && userPassword === '1234'){
+        document.getElementById('a').setAttribute('href', 'bank.html');
+    }
+    else{
+        alert('invalid user');
+    }
+})
